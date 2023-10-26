3a93994f54973d2bb562f7eb2edd10ef3fffad38
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-10-26 22:27:45 +0200

    base class for compressed data structures

---
+++ DataCompression/data_format.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <vector>
+
+template <typename T>
+class DataFormat {
+public:
+    virtual ~DataFormat() = default;
+
+    // Return decompressed data
+    virtual std::vector<T> get_data() const = 0;
+
+    // Return the size of the compressed data in memory
+    virtual size_t get_size() const = 0;
+};
