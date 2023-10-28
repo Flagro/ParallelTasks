9ec426f6f59ccd89f4cbc7bd37b5d18b82cc5e60
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-10-28 22:25:19 +0200

    debug prints

--- UniqueValues/unique_finder.cu
+++ UniqueValues/unique_finder.cu
@@ -118,11 +118,28 @@ std::vector<int> UniqueFinder::find_unique() {
     // Obtain the histogram of the data
     int blocks_count = (n + CHUNK_SIZE - 1) / CHUNK_SIZE;
     count_occurrences_kernel<<<blocks_count, BLOCK_SIZE, nunique * sizeof(int)>>>(d_data, d_histogram, n, nunique, CHUNK_SIZE);
+    // After generating the histogram
+    int* h_histogram_debug = new int[nunique];
+    cudaMemcpy(h_histogram_debug, d_histogram, nunique * sizeof(int), cudaMemcpyDeviceToHost);
+    for (int i = 0; i < nunique; i++) {
+        std::cout << "Hist[" << i << "]: " << h_histogram_debug[i] << std::endl;
+    }
+    delete[] h_histogram_debug;
+
+
 
     // Convert histogram to binary format
     int* d_binary;
     cudaMalloc(&d_binary, nunique * sizeof(int));
     histogram_to_binary<<<(nunique + BLOCK_SIZE - 1) / BLOCK_SIZE, BLOCK_SIZE>>>(d_histogram, d_binary, nunique);
+    // After converting to binary
+    int* h_binary_debug = new int[nunique];
+    cudaMemcpy(h_binary_debug, d_binary, nunique * sizeof(int), cudaMemcpyDeviceToHost);
+    for (int i = 0; i < nunique; i++) {
+        std::cout << "Binary[" << i << "]: " << h_binary_debug[i] << std::endl;
+    }
+    delete[] h_binary_debug;
+
 
     // Allocate memory for prefix_sum and unique_values on the device
     int* d_prefix_sum, *d_unique_values;
@@ -131,6 +148,13 @@ std::vector<int> UniqueFinder::find_unique() {
 
     // Compute prefix sum
     prefix_sum_kernel<<<1, BLOCK_SIZE / 2, nunique * sizeof(int)>>>(d_histogram, d_prefix_sum, nunique);
+    // After computing prefix sum
+    int* h_prefix_sum_debug = new int[nunique];
+    cudaMemcpy(h_prefix_sum_debug, d_prefix_sum, nunique * sizeof(int), cudaMemcpyDeviceToHost);
+    for (int i = 0; i < nunique; i++) {
+        std::cout << "PrefixSum[" << i << "]: " << h_prefix_sum_debug[i] << std::endl;
+    }
+    delete[] h_prefix_sum_debug;
 
     // Extract unique values based on the prefix sum
     extract_unique_values<<<(nunique + BLOCK_SIZE - 1) / BLOCK_SIZE, BLOCK_SIZE>>>(d_histogram, d_binary, d_data, d_unique_values, nunique);
