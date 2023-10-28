3acdfcd4706477c17cc6f1ae5d8c56a22cab3aab
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-10-28 03:43:35 +0200

    added switch between implementations

--- VectorCompression/compressed_data.cpp
+++ VectorCompression/compressed_data.cpp
@@ -2,11 +2,48 @@
 #include "huffman_data.hpp"
 #include "bitset_data.hpp"
 #include <vector>
+#include <cmath>
 #include <omp.h>
 
 template <typename T>
 CompressedData<T>::CompressedData(const std::vector<T>& input, T max_value) {
-    data_format_ = std::make_unique<BitsetData<T>>(input, max_value);
+    std::vector<T> frequency(max_value + 1, 0);  // Initialize with zeros
+
+    #pragma omp parallel
+    {
+        // Each thread will have its own local dense array
+        std::vector<T> local_dense_array(max_value + 1, 0);
+
+        #pragma omp for
+        for (size_t i = 0; i < input.size(); i++) {
+            local_dense_array[input[i]]++;
+        }
+
+        // Now, we'll reduce/merge the local dense arrays into the global one
+        #pragma omp critical
+        {
+            for (T i = 0; i <= max_value; i++) {
+                frequency[i] += local_dense_array[i];
+            }
+        }
+    }
+
+    double entropy = 0.0;
+    double data_size = static_cast<double>(data.size());
+
+    #pragma omp parallel for reduction(-:entropy)
+    for (size_t i = 0; i < frequency.size(); i++) {
+        double prob = static_cast<double>(frequency[i]) / data_size;
+        entropy -= prob * std::log2(prob);
+    }
+
+    double max_entropy = std::log2(max_value + 1);
+
+    if (input.size() < 1000 || entropy / max_entropy > 0.95) {
+        data_format_ = std::make_unique<BitsetData<T>>(input, max_value);
+    } else {
+        data_format_ = std::make_unique<HuffmanData<T>>(input, max_value);
+    }
 }
 
 template <typename T>
