c1dff4cb975954be7bcb95f5907588c03fea5d88
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-10-28 23:45:36 +0200

    new constants

--- UniqueValues/main.cu
+++ UniqueValues/main.cu
@@ -10,9 +10,9 @@
 #include "unique_finder.cuh"
 
 enum Constants {
-    N = 1000,       // Number of random integers
+    N = 10000000,       // Number of random integers
     T = 3,          // Number of trials
-    UNIQUE_VALUES = 700,  // limiting to 1000 unique values
+    UNIQUE_VALUES = 1000,  // limiting to 1000 unique values
     OCCUR_ONCE_CNT = 100 // Number of values that occur only once
 };
 
