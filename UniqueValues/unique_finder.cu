d0bf0c7826ddf9231c777e3eab37dc588f1388f6
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-10-28 21:48:33 +0200

    faster approach for processing

--- UniqueValues/unique_finder.cu
+++ UniqueValues/unique_finder.cu
@@ -4,10 +4,30 @@
 
 enum { BLOCK_SIZE = 256 };
 
-__global__ void count_occurrences_kernel(int* data, int* histogram, int n) {
-    int index = threadIdx.x + blockIdx.x * blockDim.x;
-    if (index < n) {
-        atomicAdd(&histogram[data[index]], 1);
+__global__ void count_occurrences_kernel(int* data, int* global_histogram, int n, int nunique, int chunk_size) {
+    extern __shared__ int local_histogram[];
+
+    int threadId = threadIdx.x;
+    int globalId = threadIdx.x + blockIdx.x * blockDim.x;
+
+    // Initialize local histogram in shared memory
+    for (int i = threadId; i < nunique; i += blockDim.x) {
+        local_histogram[i] = 0;
+    }
+    __syncthreads();
+
+    // Each thread processes a chunk of data and updates the local histogram
+    for (int i = 0; i < chunk_size; i++) {
+        int dataIdx = globalId * chunk_size + i;
+        if (dataIdx < n) {
+            atomicAdd(&local_histogram[data[dataIdx]], 1);
+        }
+    }
+    __syncthreads();
+
+    // Update global histogram from local histograms
+    for (int i = threadId; i < nunique; i += blockDim.x) {
+        atomicAdd(&global_histogram[i], local_histogram[i]);
     }
 }
 
@@ -31,8 +51,8 @@ std::vector<int> UniqueFinder::find_unique() {
     cudaMemcpy(d_data, data.data(), n * sizeof(int), cudaMemcpyHostToDevice);
     cudaMemset(d_histogram, 0, nunique * sizeof(int));
 
-    int blocks_count = (n + BLOCK_SIZE - 1) / BLOCK_SIZE;
-    count_occurrences_kernel<<<blocks_count, BLOCK_SIZE>>>(d_data, d_histogram, n);
+    int chunk_size = (n + blocks_count * BLOCK_SIZE - 1) / (blocks_count * BLOCK_SIZE);
+    count_occurrences_kernel<<<blocks_count, BLOCK_SIZE, nunique * sizeof(int)>>>(d_data, d_histogram, n, nunique, chunk_size);
 
     int* h_histogram = new int[nunique];
     cudaMemcpy(h_histogram, d_histogram, nunique * sizeof(int), cudaMemcpyDeviceToHost);
