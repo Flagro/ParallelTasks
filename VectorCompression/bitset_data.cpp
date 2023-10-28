445ad78f8611096fec88ab67258dba746ee2e0a2
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-10-28 03:34:48 +0200

    correctness fix

--- VectorCompression/bitset_data.cpp
+++ VectorCompression/bitset_data.cpp
@@ -5,23 +5,17 @@
 template <typename T>
 BitsetData<T>::BitsetData(const std::vector<T>& input_array, size_t max_value) {
     bits_per_number_ = std::ceil(std::log2(max_value + 1)); // calculate bits needed
-    compressed_data_.reserve(input_array.size() * bits_per_number_); // Reserve space
+    compressed_data_.resize(input_array.size() * bits_per_number_);
 
     #pragma omp parallel
     {
-        std::vector<bool> local_compressed;
-
         #pragma omp for nowait
         for (size_t idx = 0; idx < input_array.size(); idx++) {
             T num = input_array[idx];
+            size_t startPos = idx * bits_per_number_;
             for (int i = bits_per_number_ - 1; i >= 0; --i) {
-                local_compressed.push_back((num >> i) & 1);
+                compressed_data_[startPos + bits_per_number_ - 1 - i] = (num >> i) & 1;
             }
-        }
-
-        #pragma omp critical
-        {
-            compressed_data_.insert(compressed_data_.end(), local_compressed.begin(), local_compressed.end());
         }
     }
 }
