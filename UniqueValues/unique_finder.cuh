706daf50555608919b3eda79ad7d3558d217528a
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-10-28 16:55:07 +0200

    Revert "implemented obtaining of unique values on gpu"
    This reverts commit 7afb5628b757fc460f590e1ee60b23fda2f5bb0c.

--- UniqueValues/unique_finder.cuh
+++ UniqueValues/unique_finder.cuh
@@ -13,8 +13,6 @@ public:
 private:
     T* d_data_;
     T* d_unique_values_;
-    T* d_unique_elements_;  // This will hold unique elements on the device
-    int num_unique_elements_;  // This will hold the number of unique elements found
     T* d_histogram_;
     std::vector<T> histogram_;
     size_t unique_values_;
